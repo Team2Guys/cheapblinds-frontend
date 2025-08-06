9a5ea1ee04fa7b7428d066791c89c511a3520bc1
MFaad1 <faadsardar123@gmail.com>
2025-08-06 12:39:18 +0500

    added first commit and added configuration file

---
+++ src/app/page.test.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { render, screen } from '@testing-library/react';
+import Home from './page';
+
+describe('Home Page', () => {
+    it('renders the testing div', () => {
+        render(<Home />);
+        const divElement = screen.getByText(/testing/i);
+        expect(divElement).toBeInTheDocument();
+        expect(divElement).toHaveClass('testing');
+    });
+});
