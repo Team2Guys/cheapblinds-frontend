4189b6a288227d47fd0bf8747d66cdaa0b05ce44
MFaad1 <faadsardar123@gmail.com>
2025-08-06 16:21:11 +0500

    added changes

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,4 @@
 import '@testing-library/jest-dom';
 
-import * as React from 'react';
-import * as ReactDOMTestUtils from 'react-dom/test-utils';
 
-// Monkey-patch React.act
-(React as any).act = ReactDOMTestUtils.act;
+
