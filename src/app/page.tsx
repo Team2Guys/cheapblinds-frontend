d8e7cebc6942938dacdbd967857e7cdda0374b1b
MFaad1 <faadsardar123@gmail.com>
2025-08-07 14:58:34 +0500

    itegrated images apis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
+'use client'
+
+import ImageUploader from "components/ImageUploader/ImageUploader";
+import Image from "next/image";
+import { useState } from "react";
+import { ProductImage } from "types/prod";
+import { ImageRemoveHandler } from "utils/helperFunctions";
 
 
 export default function Home() {
+  const [images, setimages] = useState<ProductImage[] | undefined>([])
+  console.log(images, 'images')
   return (
-    <div className="testing">
-    testing123123
+    <>
+    <div className="testing flex gap-2 border border-gray-500">
+      {images?.map((val)=>
+        <Image className="border border-gray-500" key={val.public_id} src={val.imageUrl} alt="text" height={100} width={100} onClick={()=>ImageRemoveHandler(val.public_id, setimages)}/>
+      
+      )}
     </div>
+      <ImageUploader setImagesUrl={setimages} multiple/>
+    
+    </>
   );
 }
+
 
 
 
