3091fea74ddf0dd40c8b4a09bfc58af802e6f413
MFaad1 <faadsardar123@gmail.com>
2025-08-07 10:37:19 +0500

    added patch package

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,2 +1,10 @@
 // vitest.setup.ts
 import '@testing-library/jest-dom'
+import * as React from 'react';
+import * as ReactDOMTestUtils from 'react-dom/test-utils';
+
+
+if (typeof (React as any).act !== 'function') {
+  // @ts-ignore
+  React.act = ReactDOMTestUtils.act;
+}
